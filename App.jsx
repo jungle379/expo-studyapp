5e8ef253c48f562b5386fc40d0bd2afc0373288c
jungle379 <jungle379@gmail.com>
2022-06-04 16:55:43 +0900

    ⚡️

--- App.jsx
+++ App.jsx
@@ -11,11 +11,16 @@ function HomeScreen({ navigation }) {
         title="サインインする"
         onPress={() => navigation.navigate("SignIn")}
       />
+      <Text>ログインページへ</Text>
+      <Button
+        title="ログインする"
+        onPress={() => navigation.navigate("LogIn")}
+      />
     </View>
   );
 }
 
-function SignInScreen() {
+function SignInScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text style={{ padding: 10 }}>ニックネーム</Text>
@@ -26,7 +31,7 @@ function SignInScreen() {
           borderBottomWidth: 1,
           borderBottomColor: "#ccc",
         }}
-        placeholder="田中 太郎"
+        placeholder="例:田中 太郎"
       />
       <Text style={{ padding: 10 }}>メールアドレス</Text>
       <TextInput
@@ -36,7 +41,7 @@ function SignInScreen() {
           borderBottomWidth: 1,
           borderBottomColor: "#ccc",
         }}
-        placeholder="tanaka@gmail.com"
+        placeholder="例:tanaka@gmail.com"
       />{" "}
       <Text style={{ padding: 10 }}>パスワード</Text>
       <TextInput
@@ -46,17 +51,73 @@ function SignInScreen() {
           borderBottomWidth: 1,
           borderBottomColor: "#ccc",
         }}
-        placeholder="tanaka123"
+        placeholder="例:tanaka123"
       />
       <Text style={{ margin: 20 }}>登録する</Text>
+      <Button title="登録する" onPress={() => navigation.navigate("Result")} />
+      <Text style={{ margin: 20 }}>ホームへ戻る</Text>
       <Button
-        title="登録する"
-        onPress={() => navigation.navigate("HomeScreen")}
+        title="ホームへ戻る"
+        onPress={() => navigation.navigate("Home")}
+      />
+    </View>
+  );
+}
+function ResultScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>登録が完了しました！</Text>
+      <Button
+        title="メインページへ"
+        onPress={() => navigation.navigate("Index")}
+      />
+    </View>
+  );
+}
+function LogInScreen({ navigation }) {
+  return (
+    <View>
+      <Text style={{ padding: 10 }}>メールアドレス</Text>
+      <TextInput
+        style={{
+          width: "50%",
+          height: "5%",
+          borderBottomWidth: 1,
+          borderBottomColor: "#ccc",
+        }}
+        placeholder="例:tanaka@gmail.com"
+      />{" "}
+      <Text style={{ padding: 10 }}>パスワード</Text>
+      <TextInput
+        style={{
+          width: "50%",
+          height: "5%",
+          borderBottomWidth: 1,
+          borderBottomColor: "#ccc",
+        }}
+        placeholder="例:tanaka123"
+      />
+      <Text style={{ margin: 20 }}>ログイン</Text>
+      <Button
+        title="ログイン！！"
+        onPress={() => navigation.navigate("LogIn")}
       />
       <Text style={{ margin: 20 }}>ホームへ戻る</Text>
       <Button
         title="ホームへ戻る"
-        onPress={() => navigation.navigate("HomeScreen")}
+        onPress={() => navigation.navigate("Home")}
+      />
+    </View>
+  );
+}
+
+function IndexScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>サインインページへ</Text>
+      <Button
+        title="サインインする"
+        onPress={() => navigation.navigate("SignIn")}
       />
     </View>
   );
@@ -77,6 +138,21 @@ function App() {
           name="SignIn"
           component={SignInScreen}
           options={{ title: "サインインページ" }}
+        />
+        <Stack.Screen
+          name="Result"
+          component={ResultScreen}
+          options={{ title: "登録完了画面" }}
+        />
+        <Stack.Screen
+          name="LogIn"
+          component={LogInScreen}
+          options={{ title: "ログイン情報入力画面" }}
+        />
+        <Stack.Screen
+          name="Index"
+          component={IndexScreen}
+          options={{ title: "メインページ" }}
         />
       </Stack.Navigator>
     </NavigationContainer>
