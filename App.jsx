8ca5c614193c053b5a3adbf6b9a1a2002aedc46a
jungle379 <jungle379@gmail.com>
2022-06-04 14:53:03 +0900

    🔥 create_InputForm

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Button, View, Text } from "react-native";
+import { Button, View, Text, TextInput } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -18,8 +18,46 @@ function HomeScreen({ navigation }) {
 function SignInScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>ホームへ戻る</Text>
-      <Button title="戻る" onPress={() => navigation.navigate("HomeScreen")} />
+      <Text style={{ padding: 10 }}>ニックネーム</Text>
+      <TextInput
+        style={{
+          width: "50%",
+          height: "5%",
+          borderBottomWidth: 1,
+          borderBottomColor: "#ccc",
+        }}
+        placeholder="田中 太郎"
+      />
+      <Text style={{ padding: 10 }}>メールアドレス</Text>
+      <TextInput
+        style={{
+          width: "50%",
+          height: "5%",
+          borderBottomWidth: 1,
+          borderBottomColor: "#ccc",
+        }}
+        placeholder="tanaka@gmail.com"
+      />{" "}
+      <Text style={{ padding: 10 }}>パスワード</Text>
+      <TextInput
+        style={{
+          width: "50%",
+          height: "5%",
+          borderBottomWidth: 1,
+          borderBottomColor: "#ccc",
+        }}
+        placeholder="tanaka123"
+      />
+      <Text style={{ margin: 20 }}>登録する</Text>
+      <Button
+        title="登録する"
+        onPress={() => navigation.navigate("HomeScreen")}
+      />
+      <Text style={{ margin: 20 }}>ホームへ戻る</Text>
+      <Button
+        title="ホームへ戻る"
+        onPress={() => navigation.navigate("HomeScreen")}
+      />
     </View>
   );
 }
