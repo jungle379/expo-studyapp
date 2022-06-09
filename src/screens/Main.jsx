de74bba0a55b4f104c70ef07cf845f9f5c815625
jungle379 <jungle379@gmail.com>
2022-06-09 17:07:46 +0900

    ⚡️

--- src/screens/Main.jsx
+++ src/screens/Main.jsx
@@ -3,14 +3,30 @@ import { Button, View, Text } from "react-native";
 
 export function MainScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>新規投稿ページへ</Text>
-      <Button title="投稿する" onPress={() => navigation.navigate("Create")} />
-      <Text>マイページへ</Text>
-      <Button
-        title="マイページ"
-        onPress={() => navigation.navigate("MyPage")}
-      />
-    </View>
+    <>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "row",
+          alignItems: "flex-start",
+          justifyContent: "flex-end",
+          marginTop: 20,
+          paddingRight: 40,
+        }}
+      >
+        <Text></Text>
+        <Button
+          title="マイページ"
+          onPress={() => navigation.navigate("MyPage")}
+        />
+      </View>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text></Text>
+        <Button
+          title="投稿する"
+          onPress={() => navigation.navigate("Create")}
+        />
+      </View>
+    </>
   );
 }
--- src/screens/Post.jsx
+++ src/screens/Post.jsx
@@ -3,17 +3,30 @@ import { Button, View, Text } from "react-native";
 
 export function PostScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ fontSize: 60, margin: 40 }}>投稿が完了しました！</Text>
-      <Button
-        title="メインページへ"
-        onPress={() => navigation.navigate("Main")}
-      />
-      <Text>マイページへ</Text>
-      <Button
-        title="マイページ"
-        onPress={() => navigation.navigate("MyPage")}
-      />
-    </View>
+    <>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "row",
+          alignItems: "flex-start",
+          justifyContent: "flex-end",
+          marginTop: 20,
+          paddingRight: 40,
+        }}
+      >
+        <Text></Text>
+        <Button
+          title="マイページ"
+          onPress={() => navigation.navigate("MyPage")}
+        />
+      </View>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text style={{ fontSize: 60, margin: 40 }}>投稿が完了しました！</Text>
+        <Button
+          title="メインページへ"
+          onPress={() => navigation.navigate("Main")}
+        />
+      </View>
+    </>
   );
 }
