2a9919f01371f93f7183bc6d1ff75465dcf0dc67
jungle379 <jungle379@gmail.com>
2022-06-11 06:25:01 +0900

    🎨

--- src/screens/MyPage.jsx
+++ src/screens/MyPage.jsx
@@ -3,10 +3,19 @@ import { Button, View, Text } from "react-native";
 
 export function MyPageScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>新規投稿ページへ</Text>
+    <View
+      style={{
+        flex: 1,
+        flexDirection: "row",
+        alignItems: "flex-start",
+        justifyContent: "flex-end",
+        marginTop: 20,
+        paddingRight: 40,
+      }}
+    >
+      <Text></Text>
       <Button title="投稿する" onPress={() => navigation.navigate("Create")} />
-      <Text>ホームへ</Text>
+      <Text></Text>
       <Button title="戻る" onPress={() => navigation.navigate("Main")} />
     </View>
   );
