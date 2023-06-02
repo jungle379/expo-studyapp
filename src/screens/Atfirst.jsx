c2e36e2af8bc77cbd6e24362965e8a08309084a7
jungle379 <jungle379@gmail.com>
2023-06-02 14:27:13 +0900

    ⚡️

--- src/screens/Atfirst.jsx
+++ src/screens/Atfirst.jsx
@@ -1,30 +1,51 @@
 import * as React from "react";
-import { View } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 
-export function AtfirstScreen({ navigation }) {
+const Item = [
+  { id: 1, title: "馬券は基本、単複勝負" },
+  { id: 2, title: "上位人気から勝ち馬を見抜いて勝負する" },
+  { id: 3, title: "穴馬が好走するローカル競馬場は避ける" },
+  { id: 4, title: "損益を考えて、深みにハマる自滅は防ぐ" },
+  {
+    id: 5,
+    title: "近走の結果より、血統・バイアスを重視",
+  },
+  {
+    id: 6,
+    title: "重賞より、条件戦で稼ぐ",
+  },
+  { id: 7, title: "騎手・競馬場の特徴も認識の上、予想する" },
+];
+
+export function AtfirstScreen() {
   return (
     <>
       <Header />
       <View
         style={{
           flex: 3,
-          flexDirection: "row",
+          // flexDirection: "row",
           alignItems: "flex-start",
-          justifyContent: "flex-end",
+          // justifyContent: "flex-end",
           paddingRight: 40,
-          paddingTop: 20,
+          paddingLeft: 40,
+          paddingTop: 30,
           backgroundColor: "lightgreen",
         }}
       >
-        <Text>
-          馬券は基本、単複勝負 上位人気から勝ち馬を見抜いて勝負する
-          穴馬が好走するローカル競馬場は避ける
-          損益を考えて、深みにハマる自滅は防ぐ
-          近走の結果より、血統・バイアスを重視 重賞より、条件戦で稼ぐ
-          騎手・競馬場の特徴も認識の上、予想する
-        </Text>
+        <FlatList
+          data={Item}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => {
+            return (
+              <View style={{ paddingTop: 10 }}>
+                <Text style={{ fontWeight: 700 }}>・{item.title}</Text>
+              </View>
+            );
+          }}
+        />
       </View>
       <Footer />
     </>
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,12 +1,33 @@
 import * as React from "react";
-import { Button, View } from "react-native";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
+import { Button, View, Text } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome5";
 
 export function HomeScreen({ navigation }) {
   return (
     <>
-      <Header />
+      <View
+        style={{
+          alignItem: "flex-start",
+          flexDirection: "row",
+          padding: 20,
+          backgroundColor: "lightblue",
+        }}
+      >
+        <Icon name="horse-head" size={50} />
+        <Button
+          style={{
+            fontSize: 15,
+            paddingTop: 20,
+            paddingRight: 20,
+            paddingLeft: 200,
+            fontWeight: "bold",
+            color: "black",
+            textAlign: "right",
+          }}
+          title="はじめに"
+          onPress={() => navigation.navigate("Atfirst")}
+        />
+      </View>
       <View
         style={{
           flex: 3,
@@ -17,13 +38,27 @@ export function HomeScreen({ navigation }) {
           paddingTop: 20,
           backgroundColor: "lightgreen",
         }}
+      ></View>
+      <View
+        style={{
+          flex: 1,
+          alignItems: "flex-start",
+          justifyContent: "space-evenly",
+          flexDirection: "row",
+          backgroundColor: "goldenrod",
+        }}
       >
-        <Button
-          title="はじめに"
-          onPress={() => navigation.navigate("Atfirst")}
-        />
+        <Text
+          style={{
+            justifyContent: "center",
+            fontWeight: 700,
+            fontSize: 15,
+            marginTop: 20,
+          }}
+        >
+          built in 2023
+        </Text>
       </View>
-      <Footer />
     </>
   );
 }
